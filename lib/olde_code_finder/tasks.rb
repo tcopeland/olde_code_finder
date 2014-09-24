6eed97031491497ef357cdeeca3d3a105d2b592c
Tom Copeland <tom.copeland@livingsocial.com>
2014-09-24 13:24:05 -0400

    Skip directories

--- lib/olde_code_finder/tasks.rb
+++ lib/olde_code_finder/tasks.rb
@@ -2,14 +2,14 @@ namespace :olde_code_finder do
 
   desc "Find code that's mostly by a particular person"
   task :find_by_author, [:glob, :author, :pctg] => :environment do |task, args|
-    Dir[args[:glob]].each do |file|
+    Dir[args[:glob]].select {|candidate| File.file?(candidate) }.each do |file|
       OldeCodeFinder::Finder.new(file, args[:pctg]).check_by_author(args[:author])
     end
   end
 
   desc "Find code older than a particular date"
   task :find_by_date, [:glob, :date_string, :pctg] => :environment do |task, args|
-    Dir[args[:glob]].each do |file|
+    Dir[args[:glob]].select {|candidate| File.file?(candidate) }.each do |file|
       OldeCodeFinder::Finder.new(file, args[:pctg]).check_by_date(args[:date_string])
     end
   end
--- lib/olde_code_finder/version.rb
+++ lib/olde_code_finder/version.rb
@@ -1,3 +1,3 @@
 module OldeCodeFinder
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
